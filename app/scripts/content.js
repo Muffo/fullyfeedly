4d186ce9ca426bd33a85d10a82038ae9d930b30f
Andrea Grandi <muffo87@gmail.com>
2014-09-06 16:45:31 +0100

    Code refactoring

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -4,94 +4,6 @@
 'use strict';
 
 /**
- * Performs an XMLHttpRequest to boilerpipe to get the content of the artile.
- *
- * @param callback Function If the response from fetching url has a
- *     HTTP status of 200, this function is called with a JSON decoded
- *     response.  Otherwise, this function is called with null.
- */
-function fetchPageContent(callback) {
-
-    // Search the link of the article that is currently opened
-    var linkElement = document.querySelector('.websiteCallForAction');
-    if (linkElement === null) {
-        console.log('[FullyFeedly] Link to article not found');
-        iosOverlay({
-            text: 'Article not found',
-            duration: 2e3,
-            icon: chrome.extension.getURL('images/cross.png')
-        });
-        return;
-    }
-
-    // Get the link and convert it for usage as parameter in the GET request
-    var pageUrl = linkElement.getAttribute('href');
-    var encodedPageUrl = encodeURIComponent(pageUrl);
-
-    // Prepare the request to Boilerpipe
-    var url = 'http://boilerpipe-web.appspot.com/extract?url=' +
-                encodedPageUrl +
-                '&extractor=ArticleExtractor&output=json&extractImages=';
-	
-    // Spienner options
-    var spinOpts = {
-		lines: 13,  // The number of lines to draw
-		length: 11, // The length of each line
-		width: 5,   // The line thickness
-		radius: 17, // The radius of the inner circle
-		corners: 1, // Corner roundness (0..1)
-		rotate: 0,  // The rotation offset
-		color: '#FFF', // #rgb or #rrggbb
-		speed: 1,   // Rounds per second
-		trail: 60,  // Afterglow percentage
-		shadow: false, // Whether to render a shadow
-		hwaccel: false, // Whether to use hardware acceleration
-		className: 'spinner', // The CSS class to assign to the spinner
-		zIndex: 2e9, // The z-index (defaults to 2000000000)
-		top: 'auto', // Top position relative to parent in px
-		left: 'auto' // Left position relative to parent in px
-	};
-    
-    // Create the spinner and the overlay
-	var target = document.createElement('div');
-	document.body.appendChild(target);
-	var spinner = new Spinner(spinOpts).spin(target);
-    var overlay = iosOverlay({
-        text: 'Loading',
-        duration: 2e3,
-        spinner: spinner
-    });
-   
-    // Create the asynch HTTP request 
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                // Operation succeded
-                var data = JSON.parse(xhr.responseText);
-                callback(data, overlay);
-            } else if (xhr.status === 503) {
-                console.log('[FullyFeedly] Boilerpipe API exceeded quota');
-                overlay.update({
-                    text: 'API over quota',
-                    duration: 3e3,
-                    icon: chrome.extension.getURL('images/cross.png')
-                });
-            } else {
-                console.log('[FullyFeedly] Failed to load the content of the page');
-                overlay.update({
-                    text: 'Article not loaded',
-                    duration: 3e3,
-                    icon: chrome.extension.getURL('images/cross.png')
-                });
-            }
-        }
-    };
-    xhr.open('GET', url, true);
-    xhr.send();
-}
-
-/**
  * Process the content of the article and add it to the page 
  *
  * @param data Object JSON decoded response.  Null if the request failed.
@@ -145,12 +57,113 @@ function onArticleExtracted(data, overlay) {
     });
 }
 
+function boilerpipeRequest(xhr, overlay) {
+    return function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                // Operation succeded
+                var data = JSON.parse(xhr.responseText);
+                onArticleExtracted(data, overlay);
+            } else if (xhr.status === 503) {
+                console.log('[FullyFeedly] Boilerpipe API exceeded quota');
+                overlay.update({
+                    text: 'API over quota',
+                    duration: 3e3,
+                    icon: chrome.extension.getURL('images/cross.png')
+                });
+            } else {
+                console.log('[FullyFeedly] Failed to load the content of the page');
+                overlay.update({
+                    text: 'Article not loaded',
+                    duration: 3e3,
+                    icon: chrome.extension.getURL('images/cross.png')
+                });
+            }
+        }
+    };
+}
+
+function createLoadingOverlay() {
+
+    // Spinner options
+    var spinOpts = {
+		lines: 13,  // The number of lines to draw
+		length: 11, // The length of each line
+		width: 5,   // The line thickness
+		radius: 17, // The radius of the inner circle
+		corners: 1, // Corner roundness (0..1)
+		rotate: 0,  // The rotation offset
+		color: '#FFF', // #rgb or #rrggbb
+		speed: 1,   // Rounds per second
+		trail: 60,  // Afterglow percentage
+		shadow: false, // Whether to render a shadow
+		hwaccel: false, // Whether to use hardware acceleration
+		className: 'spinner', // The CSS class to assign to the spinner
+		zIndex: 2e9, // The z-index (defaults to 2000000000)
+		top: 'auto', // Top position relative to parent in px
+		left: 'auto' // Left position relative to parent in px
+	};
+    
+    // Create the spinner and the overlay
+	var target = document.createElement('div');
+	document.body.appendChild(target);
+	var spinner = new Spinner(spinOpts).spin(target);
+    var overlay = iosOverlay({
+        text: 'Loading',
+        duration: 2e3,
+        spinner: spinner
+    });
+
+    return overlay;
+}
+
+/**
+ * Performs an XMLHttpRequest to boilerpipe to get the content of the artile.
+ *
+ * @param callback Function If the response from fetching url has a
+ *     HTTP status of 200, this function is called with a JSON decoded
+ *     response.  Otherwise, this function is called with null.
+ */
+function fetchPageContent() {
+
+    // Search the link of the article that is currently opened
+    var linkElement = document.querySelector('.websiteCallForAction');
+    if (linkElement === null) {
+        console.log('[FullyFeedly] Link to article not found');
+        iosOverlay({
+            text: 'Article not found',
+            duration: 2e3,
+            icon: chrome.extension.getURL('images/cross.png')
+        });
+        return;
+    }
+
+    // Get the link and convert it for usage as parameter in the GET request
+    var pageUrl = linkElement.getAttribute('href');
+    var encodedPageUrl = encodeURIComponent(pageUrl);
+
+    // Prepare the request to Boilerpipe
+    var url = 'http://boilerpipe-web.appspot.com/extract?url=' +
+                encodedPageUrl +
+                '&extractor=ArticleExtractor&output=json&extractImages=';
+
+    var overlay = createLoadingOverlay();
+   
+    // Create the asynch HTTP request 
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = boilerpipeRequest(xhr, overlay);
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
+
+
 /* Listen for requests */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     // If the received message has the expected format...
     if (msg.text && (msg.text === 'extract_article')) {
         // Perform the specified operation
-        fetchPageContent(onArticleExtracted);
+        fetchPageContent();
         sendResponse('Got it!');
     }
 });
