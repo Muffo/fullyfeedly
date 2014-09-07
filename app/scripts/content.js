392c99ccb87cab1c502827bbc3fe3fb65e4c4055
Andrea Grandi <muffo87@gmail.com>
2014-09-07 17:32:35 +0100

    Experiment with DOM Observer

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -275,4 +275,32 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     }
 });
 
+/* ================ DOM Observer =============== */
+var observeDOM = (function() {
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    var eventListenerSupported = window.addEventListener;
 
+    return function(obj, callback) {
+        if (MutationObserver) {
+             // define a new observer
+            var obs = new MutationObserver(function(mutations, observer) {
+                console.log(observer);
+                if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
+                    callback(mutations);
+                }
+            });
+            // have the observer observe foo for changes in children
+            obs.observe(obj, { childList:true, subtree:true });
+        }
+        else if(eventListenerSupported) {
+            obj.addEventListener('DOMNodeInserted', callback, false);
+            obj.addEventListener('DOMNodeRemoved', callback, false);
+        }
+    };
+})();
+
+// Observe a specific DOM element:
+observeDOM(document.getElementById('box'), function(mutations) {
+    console.log('[FullyFeedly] DOM changed');
+    console.log('[FullyFeedly] ', mutations);
+});
