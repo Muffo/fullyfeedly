b59dba8e3b76789da8e9a5b44731468d921beab9
Andrea Grandi <muffo87@gmail.com>
2019-03-30 10:48:53 -0700

    Update URL of Mercury API
    Postlight is retiring the original Mercury API. The code has been
    released under an open source license and has been deployed on AWS.
    The extension requires updated permissions.
    
    Fix #46

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -5,13 +5,7 @@
 // Update the form showing the optional fields
 function updateForm() {
     var extractionAPI = $('#extractionAPI').val();
-
-    if (extractionAPI === 'Mercury') {
-        $('#mercuryKeyForm').show();
-    }
-    else {
-        $('#mercuryKeyForm').hide();
-    }
+    // Nothing to do here for now...
 }
 
 // Show a visual confirmation to the user
@@ -25,27 +19,11 @@ function onKeyboardShortcut() {
 // Saves options to chrome.storage
 function saveOptions() {
     var extractionAPI = $('#extractionAPI').val();
-    var mercuryAPIKey = $('#mercuryAPIKey').val();
     var enableShortcut = $('#enableShortcut').prop('checked');
     var status = $('#status');
 
-    // Check optional paramenters
-    if (extractionAPI === 'Mercury') {
-        if (mercuryAPIKey === '') {
-            // Show the error message
-            status.text('Missing Mercury API key');
-            $('#mercuryKeyForm').addClass('has-error');
-            setTimeout(function() {
-                status.text('');
-                $('#mercuryKeyForm').removeClass('has-error');
-            }, 2000);
-            return;
-        }
-    }
-
     chrome.storage.sync.set({
         extractionAPI: extractionAPI,
-        mercuryAPIKey: mercuryAPIKey,
         enableShortcut: enableShortcut
     }, function() {
         // Update status to let user know options were saved.
@@ -58,10 +36,9 @@ function saveOptions() {
 
 // Restores the options using the preferences stored in chrome.storage.
 function restoreOptions() {
-    // Use default value extractionAPI = 'Boilerpipe' and mercuryAPIKey = ''
+    // Use default value extractionAPI = 'Mercury'
     chrome.storage.sync.get({
-        extractionAPI: 'Boilerpipe',
-        mercuryAPIKey: '',
+        extractionAPI: 'Mercury',
         enableShortcut: false
     }, function(items) {
         // In case the user has not switched to Mercury yet...
@@ -69,7 +46,6 @@ function restoreOptions() {
             items.extractionAPI = "Boilerpipe";
 
         $('#extractionAPI').val(items.extractionAPI);
-        $('#mercuryAPIKey').val(items.mercuryAPIKey);
         $('#enableShortcut').prop('checked', items.enableShortcut);
         updateForm();
     });
